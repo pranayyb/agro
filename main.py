c5bda985dfa5c8b0f4e263f65455b3e6f0f8b59a
Pranay Buradkar <pranayyb@Pranays-Air.local>
2024-11-14 11:15:12 +0530

    changed main

--- main.py
+++ main.py
@@ -30,115 +30,115 @@ plant_diseases_to_fertilizers = {
         "fertilizer": "Nitrogen-rich fertilizers",
         "treatment": "Fungicides containing captan or sulfur",
     },
-    "Apple Leaf": {
+    "Apple leaf": {
         "fertilizer": "Balanced NPK fertilizers (10-10-10)",
         "treatment": "Fungicides such as myclobutanil or mancozeb",
     },
-    "Apple Rust Leaf": {
+    "Apple rust leaf": {
         "fertilizer": "Phosphorus-rich fertilizers",
         "treatment": "Fungicides containing triadimefon or sulfur",
     },
-    "Bell Pepper Leaf Spot": {
+    "Bell_pepper leaf spot": {
         "fertilizer": "Balanced NPK fertilizer (10-10-10)",
         "treatment": "Copper-based fungicides or mancozeb",
     },
-    "Bell Pepper Leaf": {
+    "Bell_pepper leaf": {
         "fertilizer": "Nitrogen-rich fertilizers",
         "treatment": "Fungicides such as chlorothalonil",
     },
-    "Blueberry Leaf": {
+    "Blueberry leaf": {
         "fertilizer": "Acidic fertilizers (Ammonium sulfate)",
         "treatment": "Fungicides containing sulfur or copper",
     },
-    "Cherry Leaf": {
+    "Cherry leaf": {
         "fertilizer": "Balanced fertilizer with a focus on potassium",
         "treatment": "Fungicides containing copper or neem oil",
     },
-    "Corn Gray Leaf Spot": {
+    "Corn Gray leaf spot": {
         "fertilizer": "High-nitrogen fertilizer",
         "treatment": "Fungicides containing strobilurin or chlorothalonil",
     },
-    "Corn Leaf Blight": {
+    "Corn leaf blight": {
         "fertilizer": "Phosphorus-rich fertilizers",
         "treatment": "Fungicides with azoxystrobin or propiconazole",
     },
-    "Corn Rust Leaf": {
+    "Corn rust leaf": {
         "fertilizer": "High-nitrogen fertilizers",
         "treatment": "Fungicides containing triazole or chlorothalonil",
     },
-    "Peach Leaf": {
+    "Peach leaf": {
         "fertilizer": "Balanced NPK fertilizer with a focus on potassium",
         "treatment": "Fungicides containing copper or sulfur",
     },
-    "Potato Leaf Early Blight": {
+    "Potato leaf early blight": {
         "fertilizer": "Balanced NPK fertilizers, especially with potassium",
         "treatment": "Fungicides such as chlorothalonil or mancozeb",
     },
-    "Potato Leaf Late Blight": {
+    "Potato leaf late blight": {
         "fertilizer": "Phosphorus-rich fertilizers",
         "treatment": "Fungicides containing metalaxyl or mefenoxam",
     },
-    "Potato Leaf": {
+    "Potato leaf": {
         "fertilizer": "Balanced fertilizers (NPK 10-10-10)",
         "treatment": "Fungicides such as chlorothalonil or copper sulfate",
     },
-    "Raspberry Leaf": {
+    "Raspberry leaf": {
         "fertilizer": "Balanced NPK fertilizer (10-10-10)",
         "treatment": "Fungicides containing sulfur or copper",
     },
-    "Soybean Leaf": {
+    "Soyabean leaf": {
         "fertilizer": "Balanced NPK fertilizer (15-15-15)",
         "treatment": "Fungicides containing azoxystrobin or chlorothalonil",
     },
-    "Squash Powdery Mildew Leaf": {
+    "Squash Powdery mildew leaf": {
         "fertilizer": "High-potassium fertilizers",
         "treatment": "Fungicides such as sulfur or potassium bicarbonate",
     },
-    "Strawberry Leaf": {
+    "Strawberry leaf": {
         "fertilizer": "Balanced NPK fertilizer with an emphasis on potassium",
         "treatment": "Fungicides containing chlorothalonil or sulfur",
     },
-    "Tomato Early Blight Leaf": {
+    "Tomato Early blight leaf": {
         "fertilizer": "Balanced NPK fertilizer with high potassium",
         "treatment": "Fungicides like chlorothalonil or copper-based treatments",
     },
-    "Tomato Septoria Leaf Spot": {
+    "Tomato Septoria leaf spot": {
         "fertilizer": "Balanced NPK fertilizers (10-10-10)",
         "treatment": "Fungicides containing chlorothalonil or mancozeb",
     },
-    "Tomato Leaf Bacterial Spot": {
+    "Tomato leaf bacterial spot": {
         "fertilizer": "Balanced NPK fertilizer with a focus on potassium",
         "treatment": "Copper-based fungicides",
     },
-    "Tomato Leaf Late Blight": {
+    "Tomato leaf late blight": {
         "fertilizer": "Phosphorus-rich fertilizers",
         "treatment": "Fungicides containing mefenoxam or copper",
     },
-    "Tomato Leaf Mosaic Virus": {
+    "Tomato leaf mosaic virus": {
         "fertilizer": "Nutrient-rich fertilizers to improve plant vigor",
         "treatment": "No chemical treatment, remove infected plants",
     },
-    "Tomato Leaf Yellow Virus": {
+    "Tomato leaf yellow virus": {
         "fertilizer": "Balanced NPK fertilizers",
         "treatment": "No chemical treatment, remove infected plants",
     },
-    "Tomato Leaf": {
+    "Tomato leaf": {
         "fertilizer": "Balanced NPK fertilizer (10-10-10)",
         "treatment": "Fungicides containing chlorothalonil or copper",
     },
-    "Tomato Mold Leaf": {
+    "Tomato mold leaf": {
         "fertilizer": "High-potassium fertilizers",
         "treatment": "Fungicides like potassium bicarbonate or sulfur",
     },
-    "Tomato Two Spotted Spider Mites Leaf": {
+    "Tomato two spotted spider mites leaf": {
         "fertilizer": "Balanced NPK fertilizer",
         "treatment": "Miticides or insecticidal soaps",
     },
-    "Grape Leaf Black Rot": {
+    "grape leaf black rot": {
         "fertilizer": "Balanced NPK fertilizers with potassium",
         "treatment": "Fungicides like sulfur or copper-based treatments",
     },
-    "Grape Leaf": {
+    "grape leaf": {
         "fertilizer": "Balanced NPK fertilizers (10-10-10)",
         "treatment": "Fungicides such as copper sulfate",
     },
